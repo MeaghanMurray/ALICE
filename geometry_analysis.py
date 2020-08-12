1a2ed2240a85f0060719ada917eb76f1a533c002
Meaghan Murray <meaghan.rochell.murray@gmail.com>
2020-08-12 14:16:21 -0700

    add docstring to geometry_analysis.py

---
+++ geometry_analysis.py
@@ -0,0 +1,42 @@
+docstring
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
+"""
+This module has functions associated with analyzing the geometry of a molecule.
+
+When run as a script and given an xyz file, this script will print out the bonds. Run 
+
+$ python geometry_analysis.py --help 
+
+to see input options.
+"""
